2132216a75806dffa7ff66a49aaeffa41c239745
Christina Labay <Christina.m.labay@gmail.com>
2020-05-02 22:16:31 -0500

    add story struct and base functionality

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice2: UIButton!
     @IBOutlet weak var choice1: UIButton!
     @IBOutlet weak var storyLabel: UILabel!
+    
+    let story0 = "You see a fork in the road."
+    let opt1 = "Take a left"
+    let opt2 = "Take a right"
+    var storyBrain = StoryBrain()
+    let num = 0
+    
     @IBAction func buttonClick(_ sender: UIButton) {
+        let currSel = sender.currentTitle!
+        let dest = storyBrain.setDestination(currSel)
+        choice1.setTitle(storyBrain.getNextChoice1(dest), for: .normal)
+        choice2.setTitle(storyBrain.getNextChoice2(dest), for: .normal)
+        storyLabel.text = storyBrain.getNextStory(dest)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        storyLabel.text = story0
+        choice1.setTitle(opt1, for: .normal)
+        choice2.setTitle(opt2, for: .normal)
     }
 
 
